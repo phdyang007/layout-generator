21f338fc7189c8f45462bf5f9107923d6d18f96f
Haoyu Yang <haoyuy@nvidia.com>
2021-08-24 17:53:03 -0500

    update

--- src/LayoutGenerator.py
+++ src/LayoutGenerator.py
@@ -664,7 +664,13 @@ def draw_wire_track(cell, layer, min_cd, max_cd, min_length, max_length, min_t2t
 #for ispd 
 #0 1 200   design and mask
 #2 sraf
-def extract_shapes(layout, center, target_cell, out_cell):
+
+
+
+
+def extract_shapes(layout, center, target_cell, cell_name):
+    print("generate cell %s !"%cell_name)
+    out_cell=layout.create_cell(cell_name)
     sp = pya.ShapeProcessor()
     #all_layers = layout.layer_indexes()
     layer_sraf = layout.layer(2,0)
--- src/cutclip.py
+++ src/cutclip.py
@@ -4,7 +4,7 @@ import sys
 sys.path.append('./src')
 from LayoutGenerator import *
 import os
-
+import multiprocessing as mp 
 
 if __name__ == "__main__":
     #cf: clip center #design:gds/oas path
@@ -12,15 +12,20 @@ if __name__ == "__main__":
     layout = pya.Layout()
     layout.read(design)
     top_cell = layout.top_cell()
-
+    args=[]
     #for i in range(2):
     for i in range(len(centers)):
-        print("generate cell %g/%g !"%(i,len(centers)))
-        out_cell=layout.create_cell("via%g"%i)
-        out_cell=extract_shapes(layout, centers[i], top_cell, out_cell)
+        args.append([layout, centers[i], top_cell, "via%g"%i])
+        #out_cell=layout.create_cell("via%g"%i)
+        #out_cell=extract_shapes(layout, centers[i], top_cell, out_cell)
+#
+        #out_cell.write(os.path.join(out, "via%g.gds"%i))
 
-        out_cell.write(os.path.join(out, "via%g.gds"%i))
-
+    p=mp.Pool(mp.cpu_count())
+    p.map(extract_shapes_mp, args)
     
 
 
+def extract_shapes_mp(arg):
+    cell = extract_shapes(arg[0], arg[1], arg[2], arg[3])
+    cell.write(os.path.join("./gds/%s.gds"%arg[3]))
